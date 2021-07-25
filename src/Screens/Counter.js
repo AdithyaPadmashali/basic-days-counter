60806fc67d3612e7edea212e193c5cc0b8e89d60
AdithyaPadmashali <adithya.p2001@gmail.com>
2021-07-25 20:11:15 +0530

    To continue with database connection...

---
+++ src/Screens/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import '../Styles/Counter.css'
+
+export default function Counter() {
+
+    const [date, setDate] = useState(new Date().toString())
+    const [currentDate, setCurrentDate] = useState(new Date().toString())
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        const id = setTimeout(() => {
+            setDate(new Date().toString())
+            setCount((prev) => prev + 1);
+        }, 1000)
+        return () => {
+            clearTimeout(id);
+        }
+    }, [count])
+
+    return (
+        <div>
+            {count}<br />
+            {currentDate}<br />
+            {date}
+        </div>
+    )
+}
